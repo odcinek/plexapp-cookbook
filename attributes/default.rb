4169ac19d695b754edad4a764021dda491775b46
Marcin Sawicki <odcinek@gmail.com>
2017-09-13 17:38:55 -0700

    Updated default version

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@
 # construct the URL (and cached package file) with the helper library
 # method, plexapp_package_url, and plexapp_package_file.
 #
-default['plexapp']['version'] = '0.9.12.19.1537-f38ac80'
+default['plexapp']['version'] = '1.8.2.4209-b1d4aa012'
 
 # Specify the package URL to download here. The helper method will use
 # this instead of constructing it with the version, above, from the
